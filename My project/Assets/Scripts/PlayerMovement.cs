8f3095fbd7c18e7d0d18237536f15d8cbf2636e0
Mark Carson <markcarson.dev@gmail.com>
2022-05-24 01:02:29 -0700

    Initial commit
    Refactored code, renamed things, spread things apart, and fucked shit up. Enjoy!
    
    Note: jumping and most movement functions are in a broken state

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,52 +1,151 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector3 playerMovementInput;
-    private Vector2 playerMouseInput;
-    private float xRot;
-
+    // Global Static Variables
     [SerializeField]
     private Transform cam;
+
     [SerializeField]
-    private Rigidbody playerRB;
+    private Rigidbody rb;
+
     [SerializeField]
-    private float speed;
+    private float acceleration;
+    [SerializeField]
+    private Vector3 counterAccel;
+
+    [SerializeField]
+    private Vector3 speedLimit;
+
     [SerializeField]
     private float sensitivity;
+
     [SerializeField]
     private float jumpForce;
-    // Start is called before the first frame update
+    [SerializeField]
+    private float gravity;
+    
+    // Input Controllers
+    public PlayerInputActions playerControls;
+    private InputAction move;
+    private InputAction jump;
+    private InputAction look;
+    private InputAction fire;
+
+    // <-- Global Variables -->
+    // The direction the player "wishes" to move, described as the Vector2 of planar movement (WASD, etc)
+    private Vector2 wishDirection = Vector2.zero;
+    private Vector2 lookInput;
+    private float xRot;
+    private bool slide;
+    private Vector3 lastVel;
+    private float distToGround;
+
     void Start()
     {
-        playerRB = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;   
+        slide = false;
+    }
 
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        look = playerControls.Player.Look;
+        look.Enable();
+
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+        jump.performed += Jump;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+        move.Disable();
+        look.Disable();
+        jump.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        lookInput = look.ReadValue<Vector2>();
 
-        MovePlayer();
+        wishDirection = move.ReadValue<Vector2>();
+
         MovePlayerCamera();
     }
-    private void MovePlayer() {
-        Vector3 moveVector = transform.TransformDirection(playerMovementInput) * speed;
-        playerRB.velocity = new Vector3(moveVector.x, moveVector.y, moveVector.z);
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    // FixedUpdate is called in fixed time, once per frame. Use for physics events, keeps game the same despite FPS.
+    void FixedUpdate()
+    {
+        MovePlayer(wishDirection);
+    }
+
+    void LateUpdate() {
+      lastVel = rb.velocity;  
+    }
+    private void MovePlayer(Vector2 localWishDir) {
+        // if (wishDir != Vector2.zero) {  
+        Vector3 forward = cam.transform.forward.normalized;
+        Vector3 right = cam.transform.right.normalized;
+        forward.y = 0f;
+        right.y = 0f;
+
+        Vector3 wishDir = forward * localWishDir.y + right * localWishDir.x;
+        //     rb.AddForce(relativeWishDir * acceleration * Time.deltaTime);
+        // } else {
+        //     float counter = counterAccel.x;
+        //     if (!isGrounded()) {
+        //         counter = counterAccel.y;
+        //     } else if (slide) {
+        //         counter = counterAccel.z;
+        //     }
+        //     rb.AddForce(-rb.velocity.normalized * counter * Time.deltaTime); 
+        // }
+        rb.velocity = rb.velocity * 0.9967f * Time.deltaTime;
+
+        float currentSpeed = Vector3.Dot(rb.velocity, wishDir);
+        float addSpeed = Mathf.Clamp(acceleration * Time.deltaTime, 0, speedLimit.x - currentSpeed);
+
+        rb.velocity = rb.velocity + addSpeed * wishDir;
+
+        if (!isGrounded()) {
+            rb.AddForce(Vector3.up * gravity);
         }
+
     }
 
     private void MovePlayerCamera() {
-        xRot -= playerMouseInput.y * sensitivity;
+        xRot -= lookInput.y * sensitivity;
 
-        transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
+        transform.Rotate(0f, lookInput.x * sensitivity, 0f);
         cam.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (canJump()) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool canJump()
+    {
+        return isGrounded();
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 }
